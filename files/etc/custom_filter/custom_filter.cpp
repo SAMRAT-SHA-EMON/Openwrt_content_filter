12e25fe977ebb1f8a4922214bd5c4101856bdff1
Saim Uddin Samrat Sha Emon <saimuddinsamrat@gmail.com>
2025-02-10 04:53:22 +0800

    Update custom_filter.cpp

--- files/etc/custom_filter/custom_filter.cpp
+++ files/etc/custom_filter/custom_filter.cpp
@@ -8,7 +8,7 @@ int main() {
     std::string line;
 
     while (std::getline(file, line)) {
-        if (!line.empty() && line[0] != '#') { // Skip empty lines and comments
+        if (!line.empty() && line[0] != '#') {
             domains.push_back("address=/" + line + "/0.0.0.0\n");
         }
     }
@@ -18,19 +18,6 @@ int main() {
         dnsmasq_conf << entry;
     }
 
-    std::ifstream ip_file("/etc/custom_filter/blocklist.txt");
-    std::string ip_line;
-
-    while (std::getline(ip_file, ip_line)) {
-        if (ip_line.find("# IP Addresses") != std::string::npos) {
-            while (std::getline(ip_file, ip_line)) {
-                if (!ip_line.empty() && ip_line.find('.') != std::string::npos) {
-                    system(("iptables -A FORWARD -d " + ip_line + " -j DROP").c_str());
-                }
-            }
-        }
-    }
-    system("iptables -A FORWARD -d 104.16.123.96 -j DROP");
     system("service dnsmasq reload");
     return 0;
 }
